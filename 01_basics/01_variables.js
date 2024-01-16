4f78cf1cef0158e1c0b376e0ff16f422225ab046
Apurva Anand <apurvaanand101@gmail.com>
2024-01-16 12:46:33 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1234
+let accountEmail = "apurva@gmail.com"
+var accountPassword = "2u3232u3"
+accountCity = "Dumka"
+
+
+// accountId = 3     cannot change or assign new value to a const variable
+
+accountEmail = "apanand101@gmail.com"
+accountPassword = "12133121"
+accountCity = "Deoghar"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
